24033ba6784d74cfb5eebd071569ab81ee21c0e3
MarianaMendezCrm <mendezmariana879@gmail.com>
2025-01-23 18:35:28 -0600

    Add files via upload

---
+++ PARES/suma.java
@@ -0,0 +1,14 @@
+public class suma{
+
+    public static void main(String[] args) {
+        int suma = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) { 
+                suma += i;   
+            }
+        }
+
+        System.out.println("La suma de los números pares del 1 al 100 es: " + suma);
+    }
+}
